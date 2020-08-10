a61d2164e1f71ff4f4f5ca4ddd6e318bfcb9f340
DevSKtable <sk2010048720@gmail.com>
2020-08-10 19:26:06 +0900

    이미지 저장경로 수정 및 추가 주석삽입

--- z_project/웹캠_얼굴인식_동영상_저장_캡쳐.py
+++ z_project/웹캠_얼굴인식_동영상_저장_캡쳐.py
@@ -19,11 +19,16 @@ key = 1
 
 global capture, count, key
 
-def img_capture():
+def img_capture(folderName): # 이미지 저장 경로를 입력 파라미터로 받습니다.
     global capture, count
+    
     if(count != 0):     # 프레임수만큼 이미지로 저장합니다. count % 15 == 0 으로 설정하면 15프레임마다 1장씩 이미지를 저장할 수 있습니다.
-        cv2.imwrite("./image/frame%d.jpg" % (count), chk_img)
-        print('Saved frame%d.jpg' % count)
+        try:
+            cv2.imwrite(folderName+"frame%d.jpg" % (count), chk_img)
+            print('Saved frame%d.jpg' % count)
+        except Exception as e: # 얼굴이 잡히지 않을때 오류나는 것을 예외처리하는 함수입니다.
+            print(e)
+            
     count += 1
 
             
@@ -49,13 +54,15 @@ if capture.isOpened():
             y1 = int(detections[0, 0, i, 4] * h)
             x2 = int(detections[0, 0, i, 5] * w)
             y2 = int(detections[0, 0, i, 6] * h)
-            wi = x2-x1
-            hi = y2-y1
-            centroidx, centroidy = x1+(wi//2), y1+(hi//2)
-            if wi >= hi :
+            
+            # 정사각형 박스를 구하기 위한 구간
+            wi = x2-x1 # 가로변의 길이
+            hi = y2-y1 # 세로변의 길이
+            centroidx, centroidy = x1+(wi//2), y1+(hi//2) # 얼굴박스의 중앙점
+            if wi >= hi : # 가로변이 세로변 보다 길거나 같을 경우, 세로변의 길이를 가로변의 길이와 맞춥니다.
                 y1 = centroidy - (wi//2)
                 y2 = centroidy + (wi//2)
-            else :
+            else : # 세로변의 길이가 가로변보다 길 경우, 가로변의 길이를 세로변의 길이와 맞춥니다.
                 x1 = centroidx - (hi//2)
                 x2 = centroidx + (hi//2)
       
@@ -74,20 +81,26 @@ if capture.isOpened():
             
         elif key == 24:         # 동영상 녹화 24 = Ctrl + X
             record=True
-            video = cv2.VideoWriter("./video/record.mp4", fourcc, 30.0, (img.shape[1], img.shape[0]))
+            image_path = './image/' # 이미지사진의 기본경로
+            path_="./video/" # 동영상의 기본경로
+            now = datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S") # 이미지와 영상의 저장시작 시점을 통일하기 위해 녹화시점으로 이동
+            if str(now) not in os.listdir(image_path): # 이미지파일은 시점별로 묶어야 하기때문에 해당시점의 시각으로 폴더명 생성
+                os.mkdir(image_path + str(now))
+                image_path = image_path + str(now)+"/" # "/"를 입력하지 않을 경우, 파일이름에 해당시점이 붙어서 파일이름만 길어지게 생성됨
+            video = cv2.VideoWriter(path_+"record.mp4", fourcc, 30.0, (img.shape[1], img.shape[0]))
+            
         
         elif key == 3:          # 동영상 녹화 중지 및 영상 저장 3 = Ctrl + C
             print("Record stop")
             record = False       
             video.release()
-            now = datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
-            path_="./video/"
-            os.rename(path_+"record.mp4",path_+str(now)+".mp4")
+
+            os.rename(path_+"record.mp4",path_+str(now)+".mp4") # 정상적인 동영상 녹화 종료시 파일이름 변경
             
         if record == True:      # 동영상이 녹화되는 동안 프레임 만큼 이미지로 저장
             print("Record is running")
             video.write(result_img)
-            img_capture()
+            img_capture(image_path) # 이미지 저장 경로를 입력파라미터로 전달
 else:
     print("Camera is not opened")
             
