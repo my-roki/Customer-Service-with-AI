364a2cffa6c46de68948831123beb4bf613f3f04
Hui <66448232+dev-hui@users.noreply.github.com>
2020-08-14 17:43:36 +0900

    about 페이지 추가

--- KBCC Web/app.py
+++ KBCC Web/app.py
@@ -20,6 +20,9 @@ def service():
 def contact():
   return render_template('contact.html') 
 
+@app.route('/about')
+def about():
+  return render_template('about.html')
   
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=80)
