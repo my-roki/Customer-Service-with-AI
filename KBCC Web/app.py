440b81159013e99c1955c62ba731f56dbbfc9b25
Hui <66448232+dev-hui@users.noreply.github.com>
2020-08-14 16:08:22 +0900

    0814 web

---
+++ KBCC Web/app.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+  return render_template('login.html')
+  
+@app.route('/home')
+def home():
+  return render_template('home.html') 
+
+@app.route('/service')
+def service():
+  return render_template('service.html') 
+  
+@app.route('/contact')
+def contact():
+  return render_template('contact.html') 
+
+  
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=80)
