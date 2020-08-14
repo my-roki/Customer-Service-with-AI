03620118cd95e80742370d0f4f6673dc3cc00683
COM-PC\COM <dbsckdfhr123@gmail.com>
2020-08-14 10:12:46 +0900

    2020.08.14 모델 완선된거 최종 수정해서 올립니다.

--- z_project/Keras_Architecture_Practice2.py
+++ z_project/Keras_Architecture_Practice2.py
@@ -1,12 +1,14 @@
-import numpy as np
-from sklearn.model_selection import train_test_split
-import os
-import cv2
-import matplotlib.pyplot as plt
+print("Hello Atom!")
 
-categories = os.listdir(caltech_dir)
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from sklearn.model_selection import train_test_split
+
+categories = os.listdir("./image")
 categories
-categories[1]
+
 
 len(categories)
 
@@ -18,8 +20,10 @@ Y = []
 for i in categories:
     path = "./image/" + i + "/"
     imglist = os.listdir(path)
-    label = [len(i)]
-    
+    label = [categories.index(i)]
+    print(imglist)
+    print(label)
+
     for j in imglist:
         path2 = path + j
         img = cv2.imread(path2)
@@ -30,36 +34,35 @@ for i in categories:
         
         X.append(img2)
         Y.append(label)
+X
+Y
         
 X = np.array(X)
 Y = np.array(Y)
 X.shape
 Y.shape
 
-
+Y = Y.flatten()
 
 train_image, test_image, train_label , test_label = train_test_split(X, Y)
 
-train_label.flatten()
-
-
 train_image.shape
-train_image.squeeze().shape
-
-
-
-
-
 len(train_image)
 
 test_image.shape
 len(test_label)
 
+train_label.shape
+len(train_label)
+
+test_label.shape
+len(test_label)
 
 plt.figure()
 plt.imshow(train_image[0])
 plt.colorbar()
 plt.grid(False)
+plt.xlabel(categories[train_label[0]])
 plt.show()
 
 plt.figure(figsize=(10,10))
@@ -69,12 +72,13 @@ for i in range(25):
     plt.yticks([])
     plt.grid(False)
     plt.imshow(train_image[i], cmap=plt.cm.binary)
-    #plt.xlabel(categories[train_label[i]])
+    plt.xlabel(categories[train_label[i]])
 plt.show()
 
-
+import keras
 from keras import layers
-from keras import models     
+from keras import models
+from keras import optimizers     
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3,3), activation = 'relu', input_shape=(256, 256, 3)))
@@ -85,34 +89,46 @@ model.add(layers.Conv2D(128, (3,3), activation = 'relu'))
 model.add(layers.MaxPool2D((2,2)))
 model.add(layers.Flatten())
 model.add(layers.Dense(512, activation='relu'))
-model.add(layers.Dense(5, activation='softmax'))
+model.add(layers.Dense(4, activation='softmax'))
 
-from keras import optimizers
+callback_list = [keras.callbacks.EarlyStopping(monitor = 'val_acc',
+                                                patience = 1),
+                 keras.callbacks.ModelCheckpoint(filepath='model.h5',
+                                                    monitor = 'val_loss',
+                                                    save_best_only = True)]
+
 model.compile(loss = 'sparse_categorical_crossentropy', 
-              optimizer=optimizers.RMSprop(lr=1e-4),
-              metrics=['acc'])
+              optimizer = optimizers.RMSprop(lr=1e-4),
+              metrics = ['acc'])
 
 model.summary()
 
-history = model.fit(train_image, train_label, epochs = 10)
+history = model.fit(train_image, train_label, epochs = 100, callbacks=callback_list,validation_data = (test_image,test_label))
             
 import matplotlib.pyplot as plt
 
 acc = history.history['acc']
+val_acc = history.history['val_acc']
 loss = history.history['loss']
+val_loss = history.history['val_loss']
 
-epochs = range(1, len(acc) + 1)
+epochs = range(len(acc))
 
-plt.plot(epochs, acc, 'b', label = 'Training acc')
-plt.title('Training  accuracy')
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and Validation accuracy')
 plt.legend()
+
 plt.figure()
 
-plt.plot(epochs, loss, 'b', label = 'Training loss')
-plt.title('Training and Validtion loss')
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and Validation loss')
 plt.legend()
 
 plt.show()
+
+
 
 
 model = models.Sequential()
@@ -153,17 +169,22 @@ plt.legend()
 plt.show()
 
 
-test1 = cv2.imread("./customers/fpht_small/test/rok/rok201.jpg")
+test1 = cv2.imread("C:/Users/COM/Documents/image/frame29.jpg")
+b, g, r = cv2.split(test1)    
+test1 = cv2.merge([r,g,b])  
+
+plt.figure()
+plt.imshow(test1)
+plt.show()
+
+height, width = test1.shape[:2]
+test1 = cv2.resize(test1, dsize=(256, 256), interpolation=cv2.INTER_AREA)
+
 test1.shape
 test1 = np.expand_dims(test1, axis=0)
 test1.shape
 model.predict(test1)
 
-test2= cv2.imread("./customers/fpht_small/test/sk/sk201.jpg")
-test2.shape
-test2 = np.expand_dims(test2, axis=0)
-test2.shape
-model.predict(test2)
 
 
 
