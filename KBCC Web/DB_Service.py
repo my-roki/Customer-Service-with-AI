157b5ba72747176c697e6e6eabea957e5da24c1b
Hui <66448232+dev-hui@users.noreply.github.com>
2020-08-18 09:15:14 +0900

    Revert "0818_test"
    This reverts commit 6fc73a52d51b4f9277477c4f974ef564b2e963bf.

---
+++ KBCC Web/DB_Service.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from configparser import ConfigParser
+from mysql.connector import MySQLConnection, Error
+from DB_Base import *
+
+def user_history(user_no):
+    try:
+        dbconfig = read_db_config()
+        conn = MySQLConnection(**dbconfig)
+        cursor = conn.cursor(prepared=True)
+        history_sql ="""select date_format(reg_time, '%Y-%m-%d'), product_name, order_amount, user_name
+                        from order_table as ot left join user_table as ut on ot.user_no = ut.user_no
+                        left join menu_table as mt on ot.product_no = mt.product_no
+                        where ot.user_no = %s
+                        order by reg_time DESC limit 6;
+                        """
+        history_id = (user_no,)
+        cursor.execute(history_sql, history_id)
+        rows = cursor.fetchall()
+        print('Total Row(s):', cursor.rowcount)
+        return rows
+    except Error as e:
+        print(e)
+
+    finally:
+        cursor.close()
+        conn.close()
